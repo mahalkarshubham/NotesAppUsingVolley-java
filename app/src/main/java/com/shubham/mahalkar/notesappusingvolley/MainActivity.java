959768b2a9864bff0833d7c6d3f6cb541a21f28a
Shubham Mahalkar <smahalkar@gmail.com>
2020-05-23 18:13:32 +0530

    added toolbar and style

--- app/src/main/java/com/shubham/mahalkar/notesappusingvolley/MainActivity.java
+++ app/src/main/java/com/shubham/mahalkar/notesappusingvolley/MainActivity.java
@@ -1,14 +1,44 @@
 package com.shubham.mahalkar.notesappusingvolley;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    RecyclerView rvDailyNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Notes");
+        setSupportActionBar(toolbar);
+        rvDailyNotes = findViewById(R.id.rvDailyNotes);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.btn_add, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.addNote) {
+            Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
